01c70692a76ce63a71f42ddeb25367a873b57be4
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-12 12:08:43 -0700

    chapter 1, section 9 exercise

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    //flush allows user input to appear in same line as the prompt
+    //also ensures output is completley sent to terminal before user input 
+    std::cout << "Enter your name: " << std::flush;
+
+    //captures user's input into the 'name' variable
+    //cin only works for single words
+    std::cin >> name;
+
+    //response message for cout:
+    std::cout << "Hello, nice to meet you " << name << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
