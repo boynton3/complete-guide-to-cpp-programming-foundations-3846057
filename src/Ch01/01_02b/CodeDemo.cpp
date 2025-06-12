e0ee4460bfc9ddb2491c7a76a6b5135821b3baef
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-12 09:59:11 -0700

    Ch01 example, Hello World

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    //printing two end line characters to separate output from debugger text in terminal to be more readable
+    std::cout << std::endl << std::endl;
+    return 0;
+}
