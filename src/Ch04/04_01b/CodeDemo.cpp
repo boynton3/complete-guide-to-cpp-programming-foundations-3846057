c3632df8102097bcc07459dfd36451c7ca5ffeeb
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-13 15:20:50 -0700

    Ch4, arrays example

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 
 int main(){
+
+    int enemies[4];
+    float levelDifficulty [] = {1.0, 3.5, 6.0, 10.5};
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
+
     
     std::cout << std::endl << std::endl;
     return 0;
