6ce6b83e2a7052ec47d12dc3ebd08d6f7a8ddb39
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-16 12:39:03 -0700

    ch4_06, vectors practice

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -4,8 +4,23 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+using namespace std;
 
 int main(){
+    vector<string> checkpoints = {"Start", "Forest", "Castle"};
+    checkpoints.push_back("Cave");
+    checkpoints.push_back("Finish");
+
+    cout << "The game has " << checkpoints.size() << " checkpoints"  << endl;
+    cout << "The first checkpoint is at the " << checkpoints[1] << endl;
+
+    checkpoints[1] = "Dark Forest";
+    cout << "The first checkpoint is now at the " << checkpoints[1] << endl;
+
+
+
     
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -5,9 +5,41 @@
 #include <vector>
 #include <iostream>
 #include <complex>
+using namespace std;
 
 int main(){
-    
+    // declare vector 'points' of class complex and type double 
+    vector<complex<double>> points;
+
+    points.push_back(complex<double>(3.5, 4.0));
+    points.push_back(complex<double>(2.3, -1.9));
+    points.push_back(complex<double>(-9.8, 3.2));
+    points.push_back(complex<double>(6.2, 4.6));
+
+    cout << "Points has " << points.size() << " elements." << endl;
+    std::cout << std::endl << std::endl;
+
+    cout << "The real number of element 1 is " << points[0].real() << endl;
+    cout << "The imaginary number of element 1 is " << points[0].imag() << endl;
+    std::cout << std::endl << std::endl;
+
+
+    cout << "The real number of element 2 is " << (points.begin() + 1)->real() << endl;
+    cout << "The imaginary number of element  2 is " << (points.begin() + 1)->imag() << endl;
+    std::cout << std::endl << std::endl;
+
+    //alternative ways to access vectors:
+    cout << "The real number of element 3 is " << prev(points.end(), 2)->real() << endl;
+    cout << "The imaginary number of element  3 is " << prev(points.end(), 2)->imag() << endl;
+    std::cout << std::endl << std::endl;
+
+    cout << "The real number of the last element is " << (points.end()-1)->real() << endl;
+    cout << "The imaginary number of the last element is " << (points.end()-1)->imag() << endl;
+
+   
+
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
