89ac13e4d387949c8b9524d57158bc02e08ed6e7
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-16 10:37:27 -0700

    Ch04, arrays and pointers example

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -5,10 +5,34 @@
 #include <iostream>
 
 int main(){
+    //an array of int containing high scores
     int highScores[] = {850, 745, 1220, 990};  // High scores in various game stages
-    int* scorePtr = highScores;   // Pointer initialized to the address of the array
 
-    std::cout << "Initial high score: " << *scorePtr << std::endl;  // Display the first high score using pointer
+    /*
+    - pointer that points to highScores
+    - essentially holds the address of the array's first element
+    - i.e pointer refers directly to the array and allows access to array elements within the pointer 
+    */
+
+    // Pointer initialized to the address of the array
+    int* scorePtr = highScores;
+    //also can be written as int* scorePtr = &highScores[0]; which means the address of the first element of highScores
+
+    //acess the contents of the array by dereferencing the point using *
+
+
+
+
+
+    // Display the first high score using pointer
+    std::cout << "Initial high score: " << *scorePtr << std::endl; 
+    // Display the second high score using pointer by dereferencing the value of the pointer plus 1. remember the value of the pointer is the address of the first element of the array and to access that element, you dereference it 
+    std::cout << "Second high score using pointer: " << *(scorePtr + 1) << std::endl;
+
+    // pointers can iterate through data like arrays. squre bracket operator goes to the address of the array or pointer that's in the brackets and derefereence that data address
+    std::cout << "Third high score using array notation: " << scorePtr[2] << std::endl;  
+    
+    //note that arrays are not mutable. You can assign different addresses to a pointer in runtime, but not for an array.
 
     std::cout << std::endl << std::endl;
     return 0;
