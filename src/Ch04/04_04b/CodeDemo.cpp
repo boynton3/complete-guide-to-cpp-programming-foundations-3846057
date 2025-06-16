cdbe64bd95e922ff373758a2a03c4e15435b719d
Angelina Boynton <angelinaboynton@yahoo.com>
2025-06-16 11:14:22 -0700

    Ch4_04, practice with pointers, constructors, and destructors

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -4,26 +4,37 @@
 
 #include <iostream>
 #include <string>
+//includes classes to keep track of time:
 #include <chrono>
 
 // Type aliases for chrono classes
 using Clock = std::chrono::system_clock;
 using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
+//using std::cout;
+//using std::endl;
+
 
 int main(){
+    //declares two objects of TimePoint class to record time before and after users input
     TimePoint start, end;
+    //duration object
     Duration elapsed_seconds;
+    //string for user input 
     std::string input;
-
+    //assigns current time to the 'start' object by calling the function from the system_clock class of library "chrono_system" and uses the member function 'now()'. The member function 'now()' is a static function (which is why we use '::' instead of '.' to access, that returns a std::chrono::time_point representing the current point in time. time_point is a template class which we'll get to later.
     start = Clock::now();
 
+
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
+    // waits for user input to continue. note, that this does not verify the word 'start' is input
     std::cin >> input;
 
     end = Clock::now();
+    //note that this will use the overloaded subtraction operator from the time_point class. Overloaded I think means that this class has it's own implementation for subtraction
     elapsed_seconds = end - start;
 
+    //special notation for accessing a member of an object. Here we access the elapsed_seconds member using the count() function defined by the template class duration. This function returns elapsed time as a double.
     std::cout << "Reaction time: " << elapsed_seconds.count() << "s" << std::endl;
     
     std::cout << std::endl << std::endl;
--- src/Ch04/04_04e/CodeDemo.cpp
+++ src/Ch04/04_04e/CodeDemo.cpp
@@ -12,20 +12,35 @@ using TimePoint = std::chrono::time_point<Clock>;
 using Duration = std::chrono::duration<double>;
 
 int main(){
+    //can add pointers to manage TimePoints dynamically 
     TimePoint *start, *end;
+
+    //can allocate objects memory dynamically at it's declaration using a CONSTRUCTOR
+    // a constructor always has the same name as the class and allows you to allocate memory using the new operator followed by a call to the constructor 
+    //A constructor is a special method that is automatically called when an object of a class is created.
+    // this returns a pointer to the allocated memory where the object is located 
     Duration *elapsed_seconds = new Duration();
     std::string input;
 
+    //now that these are pointers, we need to assign new memory for them since we did not allocate any at their declaration 
+    // here we can pass parameters to a constructor to set intial attributes of members 
+    //here we have the new operator, the constructor TimePoint, and we are passing it a member of the Clock class that uses the member function now().
     start = new TimePoint(Clock::now());
 
     std::cout << "Type 'start' as quickly as you can and hit ENTER: " << std::flush;
     std::cin >> input;
 
     end = new TimePoint(Clock::now());
+
+    // we then update this the subtract the pointers. Without this, it subtracts the addresses that start and end are pointing to. By dereferencing the objects, we get the values
     *elapsed_seconds = *end - *start;
 
+
+    // note, a pointer does not have member functions so we can't use '.' operator to access objects. Because it is a pointer, we need to defreference the pointer first and then access the object like " (*elapsed_seconds).count() ". Alternatively, use the member of pointer operator which does this for you. Reads as 'count' is member of pointer 'elapsed_seconds'.
     std::cout << "Reaction time: " << elapsed_seconds->count() << "s" << std::endl;
 
+    //need to delete objects to free memory to avoid memory leakage 
+    // this is a destructor
     delete start;
     delete end;
     delete elapsed_seconds;
